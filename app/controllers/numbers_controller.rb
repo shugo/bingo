3257723a0c4cd5ccc06bf3f9651e6b8e5a2ce9cf
Shugo Maeda <shugo@ruby-lang.org>
2022-12-18 08:56:07 +0900

    Basic auth

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,10 +1,8 @@
 class NumbersController < ApplicationController
+  before_action :basic_auth, except: :selected
+
   def index
     @numbers = Number.all
-    respond_to do |format|
-      format.html
-      format.json { render json: { numbers: @numbers }.to_json }
-    end
   end
 
   def create
@@ -14,5 +12,18 @@ class NumbersController < ApplicationController
   def destroy
     Number.clear
     @numbers = []
+  end
+
+  def selected
+    render json: { numbers: Number.all }.to_json
+  end
+
+  private
+
+  def basic_auth
+    return if ENV["BASIC_AUTH_USER"].blank?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :numbers, only: [:index, :create]
+  get "/numbers/selected", to: "numbers#selected"
   delete "/numbers", to: "numbers#destroy"
   resource :card, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
